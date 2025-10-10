a18d0717600942835d271d67c3b40fb179d3b028
HBWright <haybewright@gmail.com>
2025-10-09 19:31:08 -0500

    fjixed text scene
    everything but attaching the vr to the skiis i hope

--- Assets/Scripts/Ski_Movement_Scripts/SkiMovement.cs
+++ Assets/Scripts/Ski_Movement_Scripts/SkiMovement.cs
@@ -14,15 +14,20 @@ public class SkiMovement : MonoBehaviour
     public GameObject turnRight;
 
     public float turnRotation = 10f;
-    public float moveSpeed = 5f;        // base forward speed
+    public float maxSpeed = 9f;
     private float currentSpeed;         // current moving speed
+
+    void Start()
+    {
+        StartCoroutine(SlowDown());
+    }
     void Update()
     {
         /////////////// Turning Implementation ///////////////
         if (topTriggerLeft.currentBody != null && topTriggerLeft.currentBody == topTriggerRight.currentBody) // Makes sure at least the left trigger isn't null, that way it doesn't start turning for both bodies being null
         {
             GameObject sharedBody = topTriggerLeft.currentBody; // Same body so just reads once
-
+            
             if (sharedBody == turnRight)
             {
                 skis.transform.Rotate(Vector3.up, turnRotation * Time.deltaTime);
@@ -37,14 +42,32 @@ public class SkiMovement : MonoBehaviour
         /////////////// Pushing Implementation ///////////////
         if (bottomTriggerLeft.pushedL && bottomTriggerRight.pushedR)
         {
-            currentSpeed += 1f;            // Increase speed by 1
+            if (currentSpeed <= maxSpeed)
+            {
+                currentSpeed += 1f;            // Increase speed by 1
+                print("oh shit it pushed!!!");
+            }
+            else
+            {
+                currentSpeed = 10f;
+            }
             bottomTriggerLeft.pushedL = false; // Reset flags
             bottomTriggerRight.pushedR = false;
-            print("oh shit it pushed!!!");
         }
 
         // Move skis forward in the direction it is facing THIS WAS A NIGHTMARE TO SOLVE BTW!!!!!!!
         skis.transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime, Space.Self); // constant movement
+    }
 
+    IEnumerator SlowDown()
+    {
+        while (true)
+        {
+            if (currentSpeed > 4.1)
+            {
+                currentSpeed -= .2f;
+            }
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
